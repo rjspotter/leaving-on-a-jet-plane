94a133e092e04ac9272dc16e632904aabe1bf0d2
R. Potter <rjspotter@gmail.com>
2019-10-26 21:26:20 +0000

    Airports and start on flight datetime conversions

--- structure.sql
+++ structure.sql
@@ -109,13 +109,78 @@ INSERT INTO public.planes (tail_number)
 SELECT DISTINCT REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1')
 FROM flights WHERE REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1') IS NOT NULL;
 
+
+-- Airports
+CREATE SEQUENCE public.airports_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.airports (
+  id INTEGER NOT NULL DEFAULT nextval('airports_id_seq'),
+  code CHARACTER VARYING NOT NULL UNIQUE,
+  name CHARACTER VARYING NOT NULL,
+  city CHARACTER VARYING NOT NULL,
+  STATE CHARACTER VARYING NOT NULL,
+  state_name CHARACTER VARYING NOT NULL
+);
+ALTER TABLE ONLY public.airports ADD CONSTRAINT airports_pkey PRIMARY KEY (id);
+
+INSERT
+ INTO public.airports(
+ code,
+ NAME,
+ city,
+ STATE,
+ state_name
+)
+SELECT
+ DISTINCT originairportcode,
+ SPLIT_PART(origairportname, ': ', '2'),
+ origincityname,
+ originstate
+ originstatename
+FROM
+  flights
+;
+
+INSERT
+ INTO public.airports(
+ code,
+ NAME,
+ city,
+ STATE,
+ state_name
+)
+SELECT
+ DISTINCT destairportcode,
+ SPLIT_PART(destairportname, ': ', '2'),
+ destcityname,
+ deststate
+ deststatename
+FROM
+  flights
+WHERE
+  destairportcode NOT IN
+    (SELECT
+      DISTINCT code FROM airports)
+;
+
 -- Flights Cleaned
 
 CREATE TABLE public.flights_cleaned (
   TRANSACTIONID INT NOT NULL,
   FLIGHTDATE DATE,
   airline_id INTEGER NOT NULL,
-  plane_id INTEGER
+  plane_id INTEGER,
+  flightnumber INTEGER,
+  origin_id INTEGER,
+  destination_id INTEGER,
+  crsdeptime TIME WITHOUT TIME ZONE,
+  deptime TIME WITHOUT TIME ZONE,
+  depdelay INTEGER,
 );
 
 ALTER TABLE ONLY public.flights_cleaned
@@ -127,11 +192,21 @@ ADD CONSTRAINT fk_flights_cleaned_plane FOREIGN KEY (plane_id) REFERENCES public
 
 INSERT INTO flights_cleaned
 SELECT
-  TRANSACTIONID,
+  flights.TRANSACTIONID,
   TO_DATE(CAST(FLIGHTDATE AS VARCHAR), 'YYYYMMDD'),
   airlines.id,
-  planes.id
+  planes.id,
+  flights.FLIGHTNUM,
+  origin.id,
+  destination.id,
+  CAST( replace( CAST( round( (crsdeptime / 100.0), 2) AS TEXT), '.', ':') AS TIME),
+  CAST( replace( CAST( round( (deptime / 100.0), 2) AS TEXT), '.', ':') AS TIME),
+  depdelay
 FROM
   public.flights
   LEFT OUTER JOIN public.airlines ON public.flights.AIRLINECODE = public.airlines.CODE
-  LEFT OUTER JOIN public.planes ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1') = planes.tail_number;
+  LEFT OUTER JOIN public.planes ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1') = planes.tail_number
+  LEFT OUTER JOIN airports AS origin ON origin.code = flights.originairportcode
+  LEFT OUTER JOIN airports AS destination ON destination.code = flights.destairportcode
+;
+
