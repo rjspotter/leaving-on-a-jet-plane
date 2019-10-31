f050bd8e9d5b81987e3c6538d9a8cf6b66d1d341
R. Potter <rjspotter@gmail.com>
2019-10-31 01:09:36 +0000

    basic etl working

--- postgres/process.sql
+++ postgres/process.sql
@@ -90,42 +90,39 @@ SELECT
 FROM
   flights
   LEFT OUTER JOIN dim_airlines ON flights.AIRLINECODE = dim_airlines.CODE
-  LEFT OUTER JOIN dim_planes ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1') = dim_planes.tail_number
+  LEFT OUTER JOIN dim_planes ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]+', '', 'g'), '^N(.*)', '\1') = dim_planes.tail_number
   LEFT OUTER JOIN dim_airports AS origin ON origin.code = flights.originairportcode
   LEFT OUTER JOIN dim_airports AS destination ON destination.code = flights.destairportcode
 WHERE
   CAST(cancelled AS BOOLEAN) IS FALSE
-AND
-  transactionid NOT IN (SELECT transactionid FROM dim_flights)
 ;
 
 
 SELECT
   TRANSACTIONID,
   TO_DATE(CAST(FLIGHTDATE AS VARCHAR), 'YYYYMMDD') AS FLIGHTDATE,
-  dim_airlines.id,
-  dim_planes.id,
-  flights.FLIGHTNUM,
-  origin.id,
-  destination.id,
+  dim_airlines.id AS airline_id,
+  dim_planes.id AS plane_id,
+  flights.FLIGHTNUM AS flightnumber,
+  origin.id AS origin_id,
+  destination.id AS destination_id,
   CAST( replace( CAST( round( (crsdeptime / 100.0), 2) AS TEXT), '.', ':') AS TIME) AS crsdeptime,
   CAST( replace( CAST( round( (crsarrtime / 100.0), 2) AS TEXT), '.', ':') AS TIME) AS crsarrtime,
-  arrdelay,
   crselapsedtime,
   CAST(cancelled AS BOOLEAN) AS cancelled,
-  CAST(diverted AS BOOLEAN)AS diverted,
+  CAST(diverted AS BOOLEAN) AS diverted,
   CAST(SPLIT_PART(distance, ' ', '1') AS INTEGER) AS distance,
   SPLIT_PART(distance, ' ', '2') AS distance_unit
 FROM
   flights
   LEFT OUTER JOIN dim_airlines ON flights.AIRLINECODE = dim_airlines.CODE
-  LEFT OUTER JOIN dim_planes ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]', ''), '^N(.*)', '\1') = dim_planes.tail_number
+  LEFT OUTER JOIN
+    dim_planes
+    ON REGEXP_REPLACE(REGEXP_REPLACE(UPPER(TAILNUM), '[^A-Z0-9]+', '', 'g'), '^N(.*)', '\1') = dim_planes.tail_number
   LEFT OUTER JOIN dim_airports AS origin ON origin.code = flights.originairportcode
   LEFT OUTER JOIN dim_airports AS destination ON destination.code = flights.destairportcode
 WHERE
   CAST(cancelled AS BOOLEAN) IS TRUE
-AND
-  transactionid NOT IN (SELECT transactionid FROM dim_flights)
 ;
 
 --- Facts fact_flights
